3f6aac62b49fe2c0711a8c3f695c32b3f82e8cb2
Man-Q <81616738+Man-Q@users.noreply.github.com>
2024-05-31 14:36:00 +0200

    Modified and added comments to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import Header from "./components/Header"
 import Meme from "./components/Meme"
 
+/**
+ * App Component
+ * 
+ * This is the main component of the application. It serves as the root component
+ * that renders the Header and Meme components.
+ * 
+ * @returns {JSX.Element} The JSX to be rendered
+ */
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <Meme />
-        </div>
-    )
+  return (
+      // Return a div containing the Header and Meme components
+      <div>
+          <Header /> {/* Render the Header component */}
+          <Meme /> {/* Render the Meme component */}
+      </div>
+  );
 }
 
-export default  App;
+export default App; // Export the App component as the default export
